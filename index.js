e395163f198a1e1a33bfec33498176191752d2b7
William Jacobson <williamjacobson@Williams-MacBook-Pro.local>
2016-06-12 21:55:29 -0400

    HW-14 built out

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ getDocs(options)
   // open connection to a tcp line 
   client.connectTCP(metaDocs[0].identifier1);
   client.setID(1);
-  const promises = metaDocs.map(doc => getData(Number(doc.identifier3.slice(2)), 38));
+  const promises = metaDocs.map(doc => getData(163, 38));
   return Promise.all(promises);
 });
 
